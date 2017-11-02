a1c907ff73e39c6ae8e5339c99f4cf38b1f2759e
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2017-11-02 18:04:18 +0100

    Update Utils to 3.5.0

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -57,8 +57,8 @@ public class OOD {
 		Calendar c1 = Calendar.getInstance();
 		c1.set(2013, 3, 1);
 		Calendar c2 = Calendar.getInstance();
-		c2.set(2017, 9, 31);
-		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 51, c1.getTime(),
+		c2.set(2017, 10, 2);
+		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 54, c1.getTime(),
 				c2.getTime());
 		try {
 			InputStream is = OOD.class.getResourceAsStream("build.txt");
@@ -70,8 +70,14 @@ public class OOD {
 			VERSION.addDeveloper(new PersonDeveloper("mahdjoub", "jason", c.getTime()));
 
 			c = Calendar.getInstance();
+			c.set(2017, 10, 2);
+			Description d = new Description(2, 0, 0, Version.Type.Beta, 54, c.getTime());
+			d.addItem("Updating utils to 3.5.0");
+			VERSION.addDescription(d);
+
+			c = Calendar.getInstance();
 			c.set(2017, 9, 16);
-			Description d = new Description(2, 0, 0, Version.Type.Beta, 50, c.getTime());
+			d = new Description(2, 0, 0, Version.Type.Beta, 50, c.getTime());
 			d.addItem("Adding secure random functions into database wrapper");
 			d.addItem("public/private/secret keys can now be primary keys");
 			VERSION.addDescription(d);
