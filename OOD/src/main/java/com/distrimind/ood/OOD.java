ac723cc2d3650fdc479fbf61c08eb767eb7d2d69
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2018-01-31 09:05:26 +0100

    Update Utils to 3.8.0

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -57,8 +57,8 @@ public class OOD {
 		Calendar c1 = Calendar.getInstance();
 		c1.set(2013, 3, 1);
 		Calendar c2 = Calendar.getInstance();
-		c2.set(2017, 10, 28);
-		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 56, c1.getTime(),
+		c2.set(2018, 0, 31);
+		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 58, c1.getTime(),
 				c2.getTime());
 		try {
 			InputStream is = OOD.class.getResourceAsStream("build.txt");
@@ -70,8 +70,14 @@ public class OOD {
 			VERSION.addDeveloper(new PersonDeveloper("mahdjoub", "jason", c.getTime()));
 
 			c = Calendar.getInstance();
+			c.set(2018, 0, 31);
+			Description d = new Description(2, 0, 0, Version.Type.Beta, 58, c.getTime());
+			d.addItem("Updating utils to 3.8.0");
+			VERSION.addDescription(d);
+			
+			c = Calendar.getInstance();
 			c.set(2017, 11, 13);
-			Description d = new Description(2, 0, 0, Version.Type.Beta, 57, c.getTime());
+			d = new Description(2, 0, 0, Version.Type.Beta, 57, c.getTime());
 			d.addItem("Updating utils to 3.7.1");
 			d.addItem("Managing deconnection database exception/reconnection");
 			VERSION.addDescription(d);
