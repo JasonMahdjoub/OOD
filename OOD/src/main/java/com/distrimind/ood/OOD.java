e714cf2c6c7bbce9970226344008f14cf771a56a
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2018-04-11 17:57:43 +0200

    Release OOD 2.0.0 Beta 75

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -57,8 +57,8 @@ public class OOD {
 		Calendar c1 = Calendar.getInstance();
 		c1.set(2013, 3, 1);
 		Calendar c2 = Calendar.getInstance();
-		c2.set(2018, 3, 9);
-		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 74, c1.getTime(),
+		c2.set(2018, 3, 11);
+		VERSION = new Version("Object Oriented Database", "OOD", 2, 0, 0, Version.Type.Beta, 75, c1.getTime(),
 				c2.getTime());
 		try {
 			InputStream is = OOD.class.getResourceAsStream("build.txt");
@@ -71,9 +71,9 @@ public class OOD {
 
 			
 			c = Calendar.getInstance();
-			c.set(2018, 3, 9);
-			Description d = new Description(2, 0, 0, Version.Type.Beta, 74, c.getTime());
-			d.addItem("Updating utils to 3.13.4");
+			c.set(2018, 3, 11);
+			Description d = new Description(2, 0, 0, Version.Type.Beta, 75, c.getTime());
+			d.addItem("Updating utils to 3.14.0");
 			VERSION.addDescription(d);
 			
 			c = Calendar.getInstance();
