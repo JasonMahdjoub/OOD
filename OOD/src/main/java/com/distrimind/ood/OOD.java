7faaa735334e3e0a9eec4aa739fc521c9d499822
Jason <jason.mahdjoub@distri-mind.fr>
2018-11-06 15:59:36 +0100

    Update Utils to 3.21.0

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -61,7 +61,7 @@ public class OOD {
 		c1.set(2013, Calendar.APRIL, 1);
 		Calendar c2 = Calendar.getInstance();
 		c2.set(2018, Calendar.AUGUST, 1);
-		VERSION = new Version("Object Oriented Database", "OOD", (short)2, (short)0, (short)0, Version.Type.Beta, (short)86, c1.getTime(),
+		VERSION = new Version("Object Oriented Database", "OOD", (short)2, (short)0, (short)0, Version.Type.Beta, (short)87, c1.getTime(),
 				c2.getTime());
 		try {
 			InputStream is = OOD.class.getResourceAsStream("build.txt");
@@ -74,8 +74,8 @@ public class OOD {
 
 			c = Calendar.getInstance();
 			c.set(2018, Calendar.AUGUST, 1);
-			Description d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)86, c.getTime());
-			d.addItem("Updating utils to 3.19.0");
+			Description d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)87, c.getTime());
+			d.addItem("Updating utils to 3.21.0");
 			VERSION.addDescription(d);
 
 			c = Calendar.getInstance();
