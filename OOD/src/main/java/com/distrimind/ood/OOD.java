ca3aa4fd3b9b36b429fd11247cd41be1ce46e8cc
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2019-03-01 14:43:51 +0100

    Update years

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -60,8 +60,8 @@ public class OOD {
 		Calendar c1 = Calendar.getInstance();
 		c1.set(2013, Calendar.APRIL, 1);
 		Calendar c2 = Calendar.getInstance();
-		c2.set(2018, Calendar.FEBRUARY, 8);
-		VERSION = new Version("Object Oriented Database", "OOD", (short)2, (short)0, (short)0, Version.Type.Beta, (short)99, c1.getTime(),
+		c2.set(2019, Calendar.MARCH, 1);
+		VERSION = new Version("Object Oriented Database", "OOD", (short)2, (short)0, (short)0, Version.Type.Beta, (short)100, c1.getTime(),
 				c2.getTime());
 		try {
 			InputStream is = OOD.class.getResourceAsStream("build.txt");
@@ -73,20 +73,26 @@ public class OOD {
 			VERSION.addDeveloper(new PersonDeveloper("mahdjoub", "jason", c.getTime()));
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.FEBRUARY, 8);
-			Description d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)99, c.getTime());
+			c.set(2019, Calendar.MARCH, 1);
+			Description d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)100, c.getTime());
+			d.addItem("");
+			VERSION.addDescription(d);
+
+			c = Calendar.getInstance();
+			c.set(2019, Calendar.FEBRUARY, 8);
+			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)99, c.getTime());
 			d.addItem("Better support of sub fields into queries");
 			VERSION.addDescription(d);
 
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.FEBRUARY, 6);
+			c.set(2019, Calendar.FEBRUARY, 6);
 			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)98, c.getTime());
 			d.addItem("Update Utils to 3.25.1");
 			VERSION.addDescription(d);
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.FEBRUARY, 5);
+			c.set(2019, Calendar.FEBRUARY, 5);
 			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)97, c.getTime());
 			d.addItem("Security fix : disable cache for tables that use secret ou private keys");
 			d.addItem("Security improvement : add Field.disableCache property");
@@ -94,7 +100,7 @@ public class OOD {
 			VERSION.addDescription(d);
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.JANUARY, 25);
+			c.set(2019, Calendar.JANUARY, 25);
 			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)96, c.getTime());
 			d.addItem("Add function Table.removeRecord(Map keys)");
 			d.addItem("Add function Table.removeRecord(Object...keys)");
@@ -104,13 +110,13 @@ public class OOD {
 			VERSION.addDescription(d);
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.JANUARY, 18);
+			c.set(2019, Calendar.JANUARY, 18);
 			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)95, c.getTime());
 			d.addItem("Add H2 database driver");
 			VERSION.addDescription(d);
 
 			c = Calendar.getInstance();
-			c.set(2018, Calendar.JANUARY, 15);
+			c.set(2019, Calendar.JANUARY, 15);
 			d = new Description((short)2, (short)0, (short)0, Version.Type.Beta, (short)94, c.getTime());
 			d.addItem("Use shortest table's name");
 			d.addItem("Add possibility to personalize SQL table name (see annotation TableName)");
