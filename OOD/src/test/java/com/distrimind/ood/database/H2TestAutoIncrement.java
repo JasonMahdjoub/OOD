48db98a4c2cb38c58532584b25fd7f4bcb94136c
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2019-10-20 10:39:26 +0200

    Add auto increment test for H2

---
+++ OOD/src/test/java/com/distrimind/ood/database/H2TestAutoIncrement.java
@@ -0,0 +1,83 @@
+package com.distrimind.ood.database;
+/*
+Copyright or © or Copr. Jason Mahdjoub (01/04/2013)
+
+jason.mahdjoub@distri-mind.fr
+
+This software (Object Oriented Database (OOD)) is a computer program 
+whose purpose is to manage a local database with the object paradigm 
+and the java langage 
+
+This software is governed by the CeCILL-C license under French law and
+abiding by the rules of distribution of free software.  You can  use, 
+modify and/ or redistribute the software under the terms of the CeCILL-C
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info". 
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability. 
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or 
+data to be ensured and,  more generally, to use and operate it in the 
+same conditions as regards security. 
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL-C license and that you accept its terms.
+ */
+
+import com.distrimind.util.FileTools;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.File;
+import java.sql.*;
+
+/**
+ * @author Jason Mahdjoub
+ * @version 1.0
+ * @since OOD 2.3.5
+ */
+
+public class H2TestAutoIncrement {
+	@Test
+	public void testAutoIncrement() throws ClassNotFoundException, SQLException {
+		Class.forName("org.h2.Driver");
+		File databaseFile=new File("tmpH2Dir/h2");
+		FileTools.deleteDirectory(databaseFile.getParentFile());
+		databaseFile.getParentFile().mkdir();
+		Connection c=null;
+		try {
+			c= DriverManager
+				.getConnection("jdbc:h2:file:" + databaseFile.getAbsolutePath(), "SA", "");
+			Statement s=c.createStatement();
+			s.executeUpdate("CREATE  TABLE T11__(FLOATNUMBER_VALUE DOUBLE NOT NULL, INTEGERNUMBER_VALUE INTEGER NOT NULL, LONGNUMBER_VALUE BIGINT NOT NULL, SHORTNUMBER_VALUE SMALLINT NOT NULL, LONG_VALUE BIGINT NOT NULL, PK1 INTEGER NOT NULL, PK2 BIGINT AUTO_INCREMENT(1,1) NOT NULL, CONSTRAINT T11____PK PRIMARY KEY(PK1, PK2));");
+			s.close();
+			PreparedStatement ps=c.prepareStatement("INSERT INTO T11__(FLOATNUMBER_VALUE, INTEGERNUMBER_VALUE, LONGNUMBER_VALUE, SHORTNUMBER_VALUE, LONG_VALUE, PK1) VALUES('20', '21', '22', '23', '24', '25');", Statement.RETURN_GENERATED_KEYS );
+			Assert.assertEquals(ps.executeUpdate(), 1);
+			ResultSet r=ps.getGeneratedKeys();
+			Assert.assertTrue(r.next());
+			System.out.println(r.getMetaData().getColumnCount());
+			Assert.assertEquals(r.getLong(1), 1);
+			ps.close();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (c!=null) {
+				c.close();
+				FileTools.deleteDirectory(databaseFile.getParentFile());
+			}
+
+		}
+	}
+}
