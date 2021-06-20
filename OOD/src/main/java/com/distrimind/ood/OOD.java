265e505d96dc53b25ed6d3689e9f25bb11bb4212
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2021-06-20 10:32:51 +0200

    Update change historical

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -74,6 +74,7 @@ public class OOD {
 									.addItem("Fix bad using of central database backup when it is not used")
 									.addItem("Fix issue with notification of local host initialization")
 									.addItem("Fix issue : update internal table state when database becomes decentralized")
+									.addItem("Fix issue with DatabaseConfigurationsBuilder")
 					)
 					.addDescription(
 							new Description((short)3, (short)0, (short)0, Version.Type.STABLE, (short)0, "2021-05-24")
