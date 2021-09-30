029d2c6d6218d21477cff00ff78aa3d6c71301bd
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2021-09-30 22:41:41 +0200

    Update version

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -65,7 +65,7 @@ public class OOD {
 			VERSION.addCreator(new Person("mahdjoub", "jason"))
 					.addDeveloper(new PersonDeveloper("mahdjoub", "jason", "2013-04-01"))
 					.addDescription(
-							new Description((short)3, (short)1, (short)0, Version.Type.STABLE, (short)0, "2021-09-07")
+							new Description((short)3, (short)1, (short)0, Version.Type.STABLE, (short)0, "2021-09-30")
 									.addItem("Add file manager")
 									.addItem("Remove concatenated sql queries")
 									.addItem("Clean code")
