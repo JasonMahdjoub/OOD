13d57c8d776bd8730ece7fdf2c49b820d4a1ad98
Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>
2022-03-28 10:10:12 +0200

    OOD 3.1.19-STABLE
    Signed-off-by: Jason Mahdjoub <jason.mahdjoub@distri-mind.fr>

--- OOD/src/main/java/com/distrimind/ood/OOD.java
+++ OOD/src/main/java/com/distrimind/ood/OOD.java
@@ -65,7 +65,7 @@ public class OOD {
 			VERSION.addCreator(new Person("mahdjoub", "jason"))
 					.addDeveloper(new PersonDeveloper("mahdjoub", "jason", "2013-04-01"))
 					.addDescription(
-							new Description((short)3, (short)1, (short)18, Version.Type.STABLE, (short)0, "2022-03-25")
+							new Description((short)3, (short)1, (short)19, Version.Type.STABLE, (short)0, "2022-03-25")
 									.addItem(INTERNAL_CHANGE, "Update Utils to 5.22.3 STABLE")
 					)
 					.addDescription(
